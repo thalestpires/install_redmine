188fe1ee738c5e99892c6c2f4c25f91dc1221e10
Victor Campos <victorcampos@ufrj.br>
2015-02-07 18:19:51 -0200

    Update install_ruby.sh

--- scripts/install_ruby.sh
+++ scripts/install_ruby.sh
@@ -2,6 +2,6 @@
 
 echo 'Install RUBY'
 rvm requirements
-rvm install 1.9.3
-rvm use 1.9.3 --default
-rvm alias create default 1.9.3
+rvm install $1
+rvm use $1 --default
+rvm alias create default $1

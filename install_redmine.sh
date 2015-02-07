d66606d01bb68da89997a83684ac69515e63289b
Victor Campos <victor.campos@visagio.com>
2015-02-07 13:17:12 -0200

    fixing ruby version

--- install_redmine.sh
+++ install_redmine.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 echo 'Install RVM'
+apt-get -y install curl
 gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 source /usr/local/rvm/bin/rvm
 
 echo 'Install RUBY'
 rvm requirements
-rvm install 1.9.2
-rvm use 1.9.2 --default
+rvm install 1.9.3
+rvm use 1.9.3 --default
+rvm alias create default 1.9.3
+source ~/.bashrc
+ruby -v
 
 echo 'Install Redmine'
 mkdir /opt/redmine
 cd /opt/redmine
-apt-get install subversion
+apt-get install -y subversion
 svn co https://svn.redmine.org/redmine/branches/2.6-stable redmine-2.6
-apt-get install mysql-server
-apt-get install libmysqlclient-dev freetds-dev imagemagick libmagickcore-dev libmagickwand-dev libcurl4-openssl-dev apache2-threaded-dev libapr1-dev libaprutil1-dev
+apt-get install -y mysql-server
+apt-get install -y libmysqlclient-dev freetds-dev imagemagick libmagickcore-dev libmagickwand-dev libcurl4-openssl-dev apache2-threaded-dev libapr1-dev libaprutil1-dev
 cd redmine-2.6
 cd config/
 wget https://raw.githubusercontent.com/victorlcampos/install_redmine/master/templates/database.yml
@@ -27,6 +31,6 @@ bundle exec rake db:create RAILS_ENV=production
 bundle exec rake db:migrate RAILS_ENV=production
 
 echo 'Install Apache'
-apt-get install apache2
+apt-get install -y apache2
 gem install passenger
 passenger-install-apache2-module

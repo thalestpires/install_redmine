59921783a7a58d70936374bdce449ee6dd9ae8dc
raphael.vasconcelos <raphael.vasconcelos@visagio.com>
2015-08-24 13:40:41 -0300

    alterando rm do passenger

--- scripts/centos/install_apache.sh
+++ scripts/centos/install_apache.sh
@@ -10,7 +10,6 @@ passenger-install-apache2-module --auto --languages ruby
 passenger-install-apache2-module --snippet > passenger-compiled.conf
 awk '{new=$0; print old; old=new}END{print "  PassengerDefaultGroup apache\n  PassengerDefaultGroup apache"; print old}' < passenger-compiled.conf > passenger-compiled-final.conf
 sudo mv passenger-compiled-final.conf /etc/httpd/conf.modules.d/passenger-compiled.conf
-rm passenger-compiled.conf
+rm -rf passenger-compiled.conf
 
 sudo service httpd restart
-

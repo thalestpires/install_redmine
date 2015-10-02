c4414160218e24cc6eca3c2930a1bffb5493715b
filipexavier <filipexts@gmail.com>
2015-10-02 14:30:50 -0300

    Update pre-install.sh

--- scripts/centos/pre-install.sh
+++ scripts/centos/pre-install.sh
@@ -1,6 +1,6 @@
 sudo yum -y install svn git git-svn telnet
 
-sudo yum -y install gcc-c++ patch readline readline-devel zlib zlib-devel libcurl-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison 
+sudo yum -y install gcc-c++ patch readline readline-devel zlib zlib-devel libcurl-devel ImageMagick ImageMagick-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison 
 
 sudo sed --in-place "s/SELINUX=enforcing/SELINUX=permissive/g" /etc/selinux/config
 sudo setenforce 0
